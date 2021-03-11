b33aa086bff6ea53fb6c649c370c1630aaeb42ec
Amelia Haba <amelia.haba347@gmail.com>
2021-03-10 21:53:15 -0500

    commit tree f1efb4daa2986753e7ac0a4ae5aca59ce67e64cc

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,35 @@
+class Doctor
+    attr_accessor :name
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end
+
+    def appointments
+        Appointment.all.map do |appt|
+            if appt.doctor == self
+                appt
+            end
+        end
+    end
+
+    def new_appointment(date, name)
+        Appointment.new(date,name, self)
+    end
+
+    def patients
+        Appointment.all.map do |appt|
+            if appt.doctor == self
+                appt.patient
+            end
+        end
+    end 
+
+end 
